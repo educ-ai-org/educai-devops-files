c77a2ef6dcc2e4eeb7c678a1e4600f6d73d33259
Diego de Sousa <diegosousa241104@gmail.com>
2024-11-03 12:32:58 -0300

    Update script-backend.sh

--- script-backend.sh
+++ script-backend.sh
@@ -1,2 +1,2 @@
-sudo docker-compose up -d educai backend
-sudo docker-compose restart backend
+sudo docker-compose up -d educai backend1 backend2 backend3
+sudo docker-compose restart backend1 backend2 backend3
